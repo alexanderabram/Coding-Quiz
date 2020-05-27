3b3df1a3b86f005c5339d7067f869d2bf8f1fac2
AMA <alexanderabram6440@gmail.com>
2020-05-27 17:16:48 -0500

    updated time JS and formatted

--- assets/javascript/timer.js
+++ assets/javascript/timer.js
@@ -1,7 +1,3 @@
-// before clicking the button secondsLeft is displaying 0?
-// all this info is being printed to #counterDisplay
-
-//grab what's inside of the box
 var userName = document.querySelector("#userName");
 var endMsg = document.querySelector("#endMsg");
 var yourScore = document.querySelector("#yourScore");
@@ -17,26 +13,16 @@ function startTimer () {
     document.querySelector("#counterDisplay").innerHTML = secondsLeft;
     console.log(secondsLeft);
     
-    //if we run out of time
     if (secondsLeft === 0) {
       clearInterval(interval);
-      //   BOX TWO set to display: none
       document.querySelector("#boxTwo").setAttribute("style", "display: none");
-      //   BOX THREE displays
       document.querySelector("#boxThree").setAttribute("style", "display: block");
     
-    //or if we run out of questions, putting it at 5 so
-    //it doesn't cut off our final quesiton
     } else if (currentIndex === 5) {
-      //print seconds left to localStorage as "finalTime"
-      //end timer
       clearInterval(interval); 
-      //   BOX TWO set to display: none
       document.querySelector("#boxTwo").setAttribute("style", "display: none");
-      //   BOX FOUR displays
       document.querySelector("#boxFour").setAttribute("style", "display: block");
       
-      //grab score value and multiply it by seconds left
       score = ((score)*(secondsLeft));
       
       if (isNaN(score)) {
@@ -51,13 +37,11 @@ function startTimer () {
 
 
 
-// populates the leaderboard and adds to the high scores
 submitBtn.addEventListener("click", function(event) {
   event.stopPropagation();
   
   console.log("on submitBtn click print out score: " + score); 
 
-  //take in initals box value
   var initials = userName.value;
   console.log("initials" + initials);
 
@@ -67,7 +51,6 @@ submitBtn.addEventListener("click", function(event) {
   };
   console.log("finalScore" + finalScore);
   
-  // store score and initials client side in 'localStorage'
   highscores.push(finalScore);
   localStorage.setItem("highscores", JSON.stringify(highscores));
   console.log(initials, score);
