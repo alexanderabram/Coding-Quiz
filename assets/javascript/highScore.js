480a71d887743c0135086a391bc13b36ea1dd000
AMA <alexanderabram6440@gmail.com>
2020-05-27 17:17:56 -0500

    updated JS for high score functionality. Formatted.

--- assets/javascript/highScore.js
+++ assets/javascript/highScore.js
@@ -1,29 +1,24 @@
-//grab what highscores are in localStorage or just spit out ""
-
 var highscores = JSON.parse(localStorage.getItem("highscores")) || [];
 var highScorePrint = document.querySelector("#highScorePrint");
 var clearHighScore = document.querySelector("#clear");
 
-
-//populate high scores on highScore.html
-window.addEventListener("load", function(){printHighScore()});
+window.addEventListener("load", function () { printHighScore() });
 
 function printHighScore() {
-    highscores = scoresSorted(highscores, 'score');
-    // var sorted = highscores.sort(function(a, b) {return b.score-a.score});
+  highscores = scoresSorted(highscores, 'score');
 
-    for (var i = 0; i < highscores.length; i++) {
-      console.log(highscores[i].score);
-      var home = document.createElement("li"); //creates new p
-      var words = document.createTextNode(highscores[i].initials + ": " + highscores[i].score)  ; //content of p
-      home.appendChild(words);
-      highScorePrint.appendChild(home);
-    }
+  for (var i = 0; i < highscores.length; i++) {
+    console.log(highscores[i].score);
+    var home = document.createElement("li"); 
+    var words = document.createTextNode(highscores[i].initials + ": " + highscores[i].score); 
+    home.appendChild(words);
+    highScorePrint.appendChild(home);
+  }
 }
 
 //sort the scores
 function scoresSorted(array, key) {
-  return array.sort(function(a,b) {
+  return array.sort(function (a, b) {
     if (a.score < b.score) {
       return 1;
     }
@@ -33,7 +28,7 @@ function scoresSorted(array, key) {
 
 // //button named clear high score defined
 // //function to clear high scores or 'localStorage' items
-clearHighScore.addEventListener("click", function() {
-    localStorage.removeItem("highscores");
-    window.location.reload();
+clearHighScore.addEventListener("click", function () {
+  localStorage.removeItem("highscores");
+  window.location.reload();
 });
